445854148638c05d9c8e29d05b0c6cd5487527f9
DavZep <93220182+DavZep@users.noreply.github.com>
2022-04-21 13:55:40 -0700

    made buttons, count-down, correct or incorrect console.log, disabled buttons after user choice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,23 @@
+/*
+ when user clicks start game , 
+ the first Q should show and options should also be displayed 
+ count down timer should start
+ within options user should only be able to click once
+ if the user selects correct answer stop timer and 
+ display congrats screen w/ answer selected
+ Congrats screen should only display for a few seconds before repeating the steps above
+ if the user gets Qs wrong 
+ display wrong answer screen for a few seconds
+ display what the right answer should have been
+ if user runs out of time without answering the question, 
+ display out of time screen with correct answer
+ after all Qs have cycled display the total correct and incorrect answers 
+ offer the user to play again
+ */
+
 var gameContainer = document.getElementById("game-container");
+var timerId;
+var timeOutId;
 var game = {
     currentQuestion: 0,
     correctQuestions: 0,
@@ -21,14 +40,15 @@ var game = {
             a: 3
         }
     ],
+
     mainDisplay: function(){
         /*
-        creat instructions for the game and
+        create instructions for the game and
         play game button
         */
         gameContainer.innerHTML = ""
 
-        var h3 = document.createElement("h3")
+        var h3 = document.createElement("h3");
         h3.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac ut consequat semper viverra. Risus viverra adipiscing at in. Lobortis feugiat vivamus at augue. Vitae congue mauris rhoncus aenean vel elit scelerisque. "
         gameContainer.appendChild(h3);
 
@@ -38,8 +58,79 @@ var game = {
         btn.setAttribute("class", "btn btn-danger");
         btn.setAttribute("type", "button");
         gameContainer.appendChild(btn);
-    }
+    },
 
+    qDisplay: function(){
+        
+        //get current Question we are on, fetch current Q object from the questions array
+        var currentQ = this.questions[this.currentQuestion].q
+        var currentO = this.questions[this.currentQuestion].o
+        //clear the game-container
+        gameContainer.innerHTML = ""
+
+        var timerDiv = document.createElement("div");
+        timerDiv.setAttribute("id", "timer-wrap");
+        timerDiv.textContent = this.time
+        gameContainer.appendChild(timerDiv);
+        
+        //create text elm with current Question text
+        var h3 = document.createElement("h3");
+        h3.textContent = currentQ
+        gameContainer.appendChild(h3);
+
+        /**
+         
+         <div class="btn-group-vertical" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary">Left</button>
+            <button type="button" class="btn btn-primary">Middle</button>
+            <button type="button" class="btn btn-primary">Right</button>
+        </div>
+         */
+        var btnGroup = document.createElement("div");
+        btnGroup.setAttribute("class", "btn-group-vertical");
+        btnGroup.setAttribute("role", "group");
+
+        for(var i = 0; i < currentO.length; i++) {
+            var btn = document.createElement("button");
+            btn.setAttribute("class", "btn btn-primary q-o-btn")
+            btn.setAttribute("type", "button");
+            btn.setAttribute("data-oindex", i);
+            btn.textContent = currentO[i];
+            btnGroup.appendChild(btn);
+
+        }
+
+        gameContainer.appendChild(btnGroup);
+        clearInterval(timerId);
+        timerId = setInterval(game.timerDisplay, 1000);
+        //create buttons for each option 
+        //start interval, that counts down
+        //call interval function
+        //append test and buttons to game-container
+
+    },
+
+    bQDisplay: function(num){
+        gameContainer.innerHTML = ""
+        gameContainer.textContent = num
+    },
+
+    timerDisplay: function(){
+        var timerDiv = document.getElementById("timer-wrap");
+        //reduce time by 1, and display time on page
+        game.time--
+
+        timerDiv.textContent = game.time
+        //check if time is equal 0 or less than zero
+        if(game.time <= 0){
+            clearInterval(timerId);
+            game.time = 10;
+            game.bQDisplay(3)
+
+        }
+        //if true ,call bQDisplay
+        //clear our interval and reset timer variable
+    }
     
 }
 
@@ -47,8 +138,38 @@ document.addEventListener("click", function(event){
     //onclick start game
     if(event.target.id === "start-game") {
         console.log("start-game")
+        game.qDisplay();
+    }
+
+    //if option button clicked get the current question answer and 
+    console.log(event.target.className.includes("q-o-btn"))
+    if(event.target.className.includes("q-o-btn")) {
+        clearInterval(timerId);
+        game.time = 10;
+        var userAnswer = parseInt(event.target.getAttribute("data-oindex"));
+        var optionList = document.querySelectorAll(".q-o-btn")
+        console.log(optionList)
+        for(var i = 0; i < optionList.length; i++){
+            //disable other option buttons
+            optionList[i].setAttribute("disabled", true);
+        }
+        if(game.questions[game.currentQuestion].a === userAnswer){
+            console.log("correct");
+            game.bQDisplay(1)
+            game.correctQuestions++
+        }else {
+            console.log("incorrect");
+            game.bQDisplay(2)
+            game.incorrectQuestions++
+
+
+        }
 
     }
+    //compare it to the clicked button data O index
+
+    //clear interval
+    //call on function Display bQDisplay
 });
 
-game.mainDisplay()
+game.mainDisplay();
